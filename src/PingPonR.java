1100d53b8636a7a964ce3826576ac9aa69e04a27
mfellner <mfellner@student.tgm.ac.at>
2024-02-19 15:39:13 +0100

    feat(PingPongR): adding PingPongR thread implementation

--- src/PingPonR.java
+++ src/PingPonR.java
@@ -1,2 +1,26 @@
-public class PingPonR {
+/**
+ * Class PingPongR - Thread that prints "Ping" or "Pong", implements the Runnable class
+ *
+ * @author Manuel Fellner
+ * @version 2024-02-19
+ */
+public class PingPonR implements Runnable {
+    private String name;
+    private boolean running;
+
+    public PingPonR (String name)    {
+        this.name = name;
+        this.running = true;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+
+    @Override
+    public void run()   {
+        while (this.running) {
+            System.out.println(this.name + "\n");
+        }
+    }
 }
